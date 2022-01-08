fb38408a268c5088464dfcc2df403868943697dd
lanceberge <bergeron.lance6@gmail.com>
2022-01-08 18:42:51 -0500

    Output key statistics

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
+# Stock Data Retrieval
+
+## Example Use
+
+```
+$ python get.py AMZN GOOGL
+
+AMZN:
+--------------------------------------------------------------------------------
+Key Statistics:
+---------------
+Price:                3251.08
+Market cap:           1.65T
+EV / EBITDA:          28.244
+Net Margins:          5.74%
+Short Shares:         3.60M
+P/B:                  13.671547
+P/S:                  None
+forward PE:           63.09102
+Beta:                 1.095793
+Dividend Yield:       None
+
+GOOGL:
+--------------------------------------------------------------------------------
+Key Statistics:
+---------------
+Price:                2740.34
+Market cap:           1.82T
+EV / EBITDA:          20.013
+Net Margins:          29.52%
+Short Shares:         2.06M
+P/B:                  7.447669
+P/S:                  None
+forward PE:           24.375914
+Beta:                 1.058286
+Dividend Yield:       None
+~/code/stock_data #
+```
+
+## Dependencies
+
 pip install yahoofinancials
 pip install yfinance
-# pip install pandas_datareader
--- get.py
+++ get.py
@@ -1,102 +1,64 @@
 import sys
+from math import log, floor
 from yahoofinancials import YahooFinancials
 import yfinance as yf
-from math import log, floor
-import pandas as pd
-# import pandas_datareader as pdr
 
 # from https://stackoverflow.com/questions/3154460/python-human-readable-large-numbers
 def millify(n):
-    units = ['',' K',' M',' B',' T']
+    units = ['','K','M','B','T']
 
     n = float(n)
     k = 1000.0
     magnitude = int(floor(log(n, k)))
     return '%.2f%s' % (n / k**magnitude, units[magnitude])
 
+
 def format_percent(n):
+    if (n is None):
+        return "None"
+
     return "{:.2%}".format(n)
 
-def print_items_in_dict(dict, items, names):
-    for item, name in zip(items, names):
-        val = dict.get(item)
-        print(f"{name}: {val}")
 
-def stock_info(ticker):
+def print_nums(titles, nums):
+    for title, num in zip(titles, nums):
+        print("%-22s%-22s" % (title+': ', num))
+
+
+def key_statistics(ticker):
     stock = yf.Ticker(ticker)
     symbol = YahooFinancials(ticker)
 
-    print(ticker+": ")
+    print('\n'+ticker+": ")
+    print('-'*80)
 
     print("Key Statistics: ")
+    print('-'*(len("Key Statistics")+1))
+
+    names = ["Price"                   , "Market cap"]
+    vals  = [symbol.get_current_price(), millify(symbol.get_market_cap())]
+
+    # get the key statistics dictionary from YahooFinancials
     dict = symbol.get_key_statistics_data().get(ticker)
-    items = ["enterpriseToEbitda", "profitMargins", "sharesShort" , "priceToBook"]
-    names = ["EV / EBITDA"       , "Net Margins"  , "Short Shares", "P/B"]
 
-    items += ["priceToSalesTrailing12Months", "forwardPE" , "beta"]
-    names += ["P/S"                         , "forward PE", "Beta"]
+    rownames = ["enterpriseToEbitda", "profitMargins", "sharesShort" , "priceToBook"]
+    names   += ["EV / EBITDA"       , "Net Margins"  , "Short Shares", "P/B"]
 
+    rownames += ["priceToSalesTrailing12Months", "forwardPE" , "beta"]
+    names    += ["P/S"                         , "forward PE", "Beta"]
+
+    # update data to correct format
     dict.update({'profitMargins': format_percent(dict.get('profitMargins'))})
     dict.update({'sharesShort': millify(dict.get('sharesShort'))})
 
-    print_items_in_dict(dict, items, names)
+    # add values from the dictionary to vals
+    vals += [dict.get(row) for row in rownames]
 
-    ev = dict.get("enterpriseValue")
+    names += ["Dividend Yield"]
+    vals  += [format_percent(symbol.get_dividend_yield())]
 
-    print("Price: "         +str(symbol.get_current_price()))
-    print("Market cap: "    +str(millify(symbol.get_market_cap())))
-    print("Dividend yield: "+format_percent(symbol.get_dividend_yield()))
+    print_nums(names, vals)
 
-    # print("\nBalance Sheet: ")
-    # print(stock.balance_sheet)
-
-    # print("Quarterly: ")
-    # print(stock.quarterly_balance_sheet)
-
-    # print("Cash Flow: ")
-    # print(stock.cashflow)
-
-    # print("Quarterly: ")
-    # print(stock.quarterly_cashflow)
-
-    # print("Earnings: ")
-    # print(stock.earnings)
-
-    # print("Quarterly: ")
-    # print(stock.quarterly_earnings)
-
-    # print("Events: ")
-    # print(stock.calendar)
-
-    # print("Major Holders: ")
-    # print(stock.major_holders)
-
-    # print("Institutional: ")
-    # print(stock.institutional_holders)
-
-    # print("Info: ")
-    # print(stock.info.items())
-
-    # print("Price data: ")
-    # print(stock.history(period="max"))
-
-    # print("Actions: ")
-    # print(stock.actions)
-
-    # print("Dividends: ")
-    # print(stock.dividends)
-
-    # print("Financials: ")
-    # print(stock.financials)
-    # print("Quarterly: ")
-    # print(stock.quarterly_financials)
-
-    # print("News: ")
-    # print(stock.news)
-
-    ## over time data
-    # print(pdr.get_data_yahoo(ticker))
-    # data = yf.ticker(ticker).history(period='5y')
 
 def main():
     if len(sys.argv) == 1:
@@ -104,7 +66,7 @@ def main():
         return
     else:
         for ticker in sys.argv[1:]:
-            stock_info(ticker.upper())
+            key_statistics(ticker.upper())
 
 
 if __name__ == "__main__":
